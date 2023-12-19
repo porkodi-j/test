2f2c2b1d9356ac93ebbc0736069037d9539bfba5
porkodi-j <j.porkodi@gmail.com>
2023-12-19 11:45:16 +0000

    commit

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int Addition(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+
+    }
+}
